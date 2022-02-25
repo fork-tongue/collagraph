f554a23371c8f81d7e4e84be80acf49678d5d296
Berend Klein Haneveld <berend.kleinhaneveld@zimmerbiomet.com>
2022-02-25 11:14:08 +0100

    Add test (xfail) that triggers RecursionError

--- tests/test_pygui.py
+++ tests/test_pygui.py
@@ -1,3 +1,5 @@
+import pytest
+
 import pygui
 
 
@@ -12,3 +14,19 @@ def test_basic_dict_renderer():
     gui.render(element, container)
 
     assert container["children"][0] == {"type": "app"}
+
+
+@pytest.mark.xfail
+def test_lots_of_elements():
+    """Render a node with a 1000 children.
+
+    When using a recursive strategy to process fibers, this will result in a
+    stack of 1000 calls to `commit_work` which triggers a RecursionError.
+    This test makes sure that `pygui` will not trigger any RecursionError.
+    """
+    renderer = pygui.renderers.DictRenderer()
+    gui = pygui.PyGui(renderer=renderer, sync=True)
+    container = {"type": "root"}
+
+    element = pygui.create_element("app", {}, *[pygui.create_element("node")] * 1000)
+    gui.render(element, container)
