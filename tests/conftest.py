aed04989cdc1a42db550cc3a7ab0298f8f1cce13
Berend Klein Haneveld <berend.kleinhaneveld@zimmerbiomet.com>
2022-03-02 15:01:19 +0100

    Remove unneeded loop and just trigger a task

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,15 +4,14 @@ import pytest
 
 
 async def miniloop():
-    for i in range(2):
-        await asyncio.sleep(0.001)
+    await asyncio.sleep(0)
 
 
 @pytest.fixture
 def process_events():
     loop = asyncio.get_event_loop()
 
-    def process_events():
+    def run():
         loop.run_until_complete(miniloop())
 
-    yield process_events
+    yield run
