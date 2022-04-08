854df8c28718a9cd3e2ed3b7bc5fc682a9ace3ee
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-04-08 09:20:57 +0200

    Simplify component example

--- examples/component-example.py
+++ examples/component-example.py
@@ -1,93 +1,67 @@
 import pygfx as gfx
-from wgpu.gui.auto import run, WgpuCanvas
+from wgpu.gui.auto import call_later, run, WgpuCanvas
 
 from collagraph import Collagraph, Component, create_element as h, EventLoopType
 from collagraph.renderers import PygfxRenderer
 
 
-class Track(Component):
-    track_geometry = gfx.cylinder_geometry()
-    track_material = gfx.MeshPhongMaterial(color=[1, 0.5, 1])
-
-    def render(self):
-        return h(
-            "Mesh",
-            {
-                **self.props,
-                "geometry": Track.track_geometry,
-                "material": Track.track_material,
-            },
-        )
-
-
-class Scrubber(Component):
-    scrubber_geometry = gfx.sphere_geometry()
-    scrubber_material = gfx.MeshPhongMaterial(color=[0.5, 1, 0.5])
+class Button(Component):
+    geometry = gfx.box_geometry()
+    materials = {
+        "default": gfx.MeshPhongMaterial(color=[1.0, 0.5, 0.0]),
+        "pressed": gfx.MeshPhongMaterial(color=[0.0, 0.5, 0.0]),
+    }
 
     def __init__(self, props):
         super().__init__(props)
-        self._captured = False
-        self._sphere = None
-        self._mouse_pos = (0, 0)
-        self.state["position"] = [0, 0, 0]
+        self.state["pressed"] = False
+        self.state["scale"] = self.props.get("scale", [0.85] * 3)
+        self.mesh = None
 
-    def mouse_down(self, event):
-        if not self._sphere:
-            self._sphere = event.current_target
-        self._sphere.set_pointer_capture(event.pointer_id)
-        self._captured = True
-        self._mouse_pos = event.x, event.y
+    def pressed(self, event):
+        def release():
+            self.state["pressed"] = False
 
-    def mouse_move(self, event):
-        if not self._captured:
-            return
-
-        pos = event.x, event.y
-        diff = (
-            pos[0] - self._mouse_pos[0],
-            pos[1] - self._mouse_pos[1],
-        )
-
-        new_pos = [
-            self.state["position"][0],
-            self.state["position"][1],
-            self.state["position"][2] - diff[0] / 40,
-        ]
-
-        self._mouse_pos = pos
-        # FIXME: updating one element of self.state["position"] doesn't update component
-        self.state["position"] = new_pos
-
-    def mouse_up(self, event):
-        self._captured = False
+        self.state["pressed"] = True
+        call_later(2, release)
 
     def render(self):
+        material = (
+            Button.materials["pressed"]
+            if self.state["pressed"]
+            else Button.materials["default"]
+        )
         return h(
             "Mesh",
             {
-                **self.props,
-                **self.state,
-                "geometry": Scrubber.scrubber_geometry,
-                "material": Scrubber.scrubber_material,
-                "on_pointer_down": self.mouse_down,
-                "on_pointer_move": self.mouse_move,
-                "on_pointer_up": self.mouse_up,
+                "position": self.props["position"],
+                "scale": self.state["scale"],
+                "geometry": Button.geometry,
+                "material": material,
+                "on_click": self.pressed,
             },
         )
 
 
-class Slider(Component):
+class NumberPad(Component):
+    def __init__(self, props):
+        super().__init__(props)
+        self.state["columns"] = 4
+        self.state["rows"] = 5
+
     def render(self):
+        locations = []
+        for x in range(self.state["columns"]):
+            for y in range(self.state["rows"]):
+                locations.append(
+                    (
+                        x - (self.state["columns"] - 1) / 2,
+                        y - (self.state["rows"] - 1) / 2,
+                        0,
+                    )
+                )
         return h(
-            "Group",
-            self.state,
-            h(
-                Track,
-                {
-                    "scale": [0.5, 0.5, 10],
-                },
-            ),
-            h(Scrubber, {}),
+            "Group", {}, *[h(Button, {"position": position}) for position in locations]
         )
 
 
@@ -96,25 +70,19 @@ if __name__ == "__main__":
     renderer = gfx.renderers.WgpuRenderer(canvas)
 
     camera = gfx.PerspectiveCamera(60, 16 / 9)
-    camera.position.x = 15
+    camera.position.z = 7
+    camera.position.y = -2
 
     controls = gfx.OrbitControls(camera.position.clone())
     controls.add_default_event_handlers(renderer, canvas, camera)
 
     gui = Collagraph(renderer=PygfxRenderer(), event_loop_type=EventLoopType.QT)
 
-    # Should be possible to create this element
-    # by rendering JSX to dict.
-    # Jinja supports rendering templates to Python objects
-    # so could be an interesting starting place.
-    element = h(Slider, {})
-
+    element = h(NumberPad, {})
     container = gfx.Scene()
 
-    controls.update_camera(camera)
-
     def animate():
-        # print(camera.rotation)
+        controls.update_camera(camera)
         renderer.render(container, camera)
 
     gui.render(element, container, callback=lambda: canvas.request_draw(animate))
