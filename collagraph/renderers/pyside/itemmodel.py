fc4ff0e3ee6d50e555431353f9c17a83e8d7eb19
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-04-19 11:59:56 +0200

    Remove some commented code

--- collagraph/renderers/pyside/itemmodel.py
+++ collagraph/renderers/pyside/itemmodel.py
@@ -1,23 +1,7 @@
-from PySide6.QtGui import QStandardItem, QStandardItemModel
+from PySide6.QtGui import QStandardItemModel
 
 
 def insert(self, el, anchor=None):
-    assert isinstance(el, QStandardItem)
-
-    # TODO: This should probably be part of pyside_renderer's
-    # 'add_event_listener' or maybe that function needs
-    # to be mapped as well, such as the 'insert', 'remove'
-    # and 'set_attribute' methods...
-
-    # Make sure to try to connect event listeners from the
-    # list view to signals from the item model, such as
-    # 'itemChanged'.
-    # if slots := getattr(self, "slots", None):
-    #     for event_type in slots:
-    #         if signal := getattr(model, event_type, None):
-    #             for slot in slots[event_type]:
-    #                 signal.connect(slot)
-
     # TODO: list with multiple columns?
     if isinstance(self, QStandardItemModel):
         if index := getattr(el, "model_index", None):
