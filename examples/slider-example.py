e16004abb7f819be17a0470f137acc83f1eb58e7
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-04-19 14:39:52 +0200

    Add example showing slider, spinbox and progress bar

---
+++ examples/slider-example.py
@@ -0,0 +1,74 @@
+"""
+Example that shows slider, progress bar and spinbox that
+are connected to the same value.
+"""
+from observ import reactive
+from PySide6 import QtCore, QtWidgets
+
+from collagraph import Collagraph, create_element as h, EventLoopType
+from collagraph.renderers import PySideRenderer
+
+
+def Example(props):
+    def value_changed(value):
+        props["value"] = value
+
+    def tracking_changed(value):
+        props["tracking"] = value
+
+    return h(
+        "Window",
+        props,
+        h(
+            "Widget",
+            {},
+            h(
+                "Slider",
+                {
+                    "orientation": QtCore.Qt.Orientation.Horizontal,
+                    "minimum": 0,
+                    "maximum": 100,
+                    "value": props["value"],
+                    "on_value_changed": value_changed,
+                    "tracking": props["tracking"],
+                },
+            ),
+            h(
+                "CheckBox",
+                {
+                    "text": "slider tracking",
+                    "checked": props["tracking"],
+                    "on_state_changed": tracking_changed,
+                },
+            ),
+            h(
+                "SpinBox",
+                {
+                    "minimum": 0,
+                    "maximum": 100,
+                    "value": props["value"],
+                    "on_value_changed": value_changed,
+                },
+            ),
+            h(
+                "ProgessBar",
+                {
+                    "minimum": 0,
+                    "maximum": 100,
+                    "value": props["value"],
+                    "on_value_changed": value_changed,
+                },
+            ),
+        ),
+    )
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    gui = Collagraph(renderer=PySideRenderer(), event_loop_type=EventLoopType.QT)
+    state = reactive({"value": 50, "tracking": True})
+    element = h(Example, state)
+
+    gui.render(element, app)
+    app.exec()
