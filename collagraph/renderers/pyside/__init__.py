288d5c66be88f770cbc4ecefad7b156218f43c3d
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-04-28 11:01:20 +0200

    Add docstring to call_method

--- collagraph/renderers/pyside/__init__.py
+++ collagraph/renderers/pyside/__init__.py
@@ -94,6 +94,10 @@ def attr_name_to_method_name(name, setter=False):
 
 
 def call_method(method, args):
+    """Method that allows for calling setters/methods with multiple arguments
+    such as: `setColumnStretch` of `PySide6.QtWidgets.QGridLayout` which takes a
+    column and stretch argument.
+    """
     if isinstance(args, tuple):
         method(*args)
     else:
