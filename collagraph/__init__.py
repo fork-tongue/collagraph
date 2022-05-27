62bcdbbfbcba8ebc6cdafbd557a6030e9c0c31ea
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-05-27 13:52:44 +0200

    Clean up and update README (#47)
    * Remove redundant components folder
    * Simplify example
    * Update README

--- collagraph/__init__.py
+++ collagraph/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version
 
 from .collagraph import Collagraph, create_element, render_slot  # noqa: F401
-from .components import Component  # noqa: F401
+from .component import Component  # noqa: F401
 from .renderers import *  # noqa: F401, F403
 from .types import EventLoopType, VNode  # noqa: F401
 from .cgx import importer  # noqa: F401, I100
