eb223a479a88818fa5beb05f5ac93f004cdcec19
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-06-14 14:41:36 +0200

    Fix menus on windows (#56)

--- collagraph/renderers/pyside/objects/menubar.py
+++ collagraph/renderers/pyside/objects/menubar.py
@@ -3,14 +3,21 @@ from PySide6.QtWidgets import QMenu
 
 def insert(self, el: QMenu, anchor: QMenu = None):
     if anchor:
-        self.insertMenu(anchor, el)
+        action = self.insertMenu(anchor, el)
     else:
-        self.addMenu(el)
-    el.setParent(self)
+        action = self.addMenu(el)
+    # Use the window property of the menubar to set
+    # the parent of the action. Instead of keeping
+    # a hierarchy of widgets, instead the hierarchy
+    # is constructed with just the (menu)actions.
+    # Using the widgets works fine on macOS, but causes
+    # problems on Windows where menus would show on top
+    # of each other.
+    action.setParent(self.window())
 
 
 def remove(self, el: QMenu):
     el.clear()
     menu_action = el.menuAction()
     self.removeAction(menu_action)
-    el.setParent(None)
+    menu_action.setParent(None)
