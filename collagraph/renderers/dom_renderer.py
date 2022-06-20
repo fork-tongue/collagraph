57ca0b282a7e860829e05a6a6b84fd1d3c1c71ed
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-06-20 12:16:32 +0200

    Basic implementation of dom renderer

--- collagraph/renderers/dom_renderer.py
+++ collagraph/renderers/dom_renderer.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+import js
 
 from . import Renderer
 
@@ -7,31 +7,26 @@ class DomRenderer(Renderer):
     """Renderer that renders to a pyodide dom"""
 
     def create_element(self, type: str) -> dict:
-        return {"type": type}
+        element = js.window.document.createElement(type)
+        return element
 
     def insert(self, el, parent, anchor=None):
-        children = parent.setdefault("children", [])
-        anchor_idx = children.index(anchor) if anchor else len(children)
-        children.insert(anchor_idx, el)
+        if anchor:
+            parent.insertBefore(el, anchor)
+        else:
+            parent.appendChild(el)
 
     def remove(self, el, parent):
-        children = parent["children"]
-        children.remove(el)
+        parent.removeChild(el)
 
     def set_attribute(self, obj, attr: str, value):
-        attributes = obj.setdefault("attrs", {})
-        attributes[attr] = value
+        obj.setAttribute(attr, value)
 
     def remove_attribute(self, obj, attr: str, value):
-        attributes = obj["attrs"]
-        if attr in attributes:
-            del attributes[attr]
+        obj.removeAttribute(attr)
 
     def add_event_listener(self, el, event_type, value):
-        event_listeners = el.setdefault("handlers", defaultdict(set))
-        event_listeners[event_type].add(value)
+        el.addEventListener(event_type, value)
 
     def remove_event_listener(self, el, event_type, value):
-        event_listeners = el.get("handlers", None)
-        if event_listeners:
-            event_listeners[event_type].remove(value)
+        el.removeEventListener(event_type, value)
