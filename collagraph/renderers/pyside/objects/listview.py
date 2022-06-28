533a61ba90520018758627b7db6ca8a107873b9b
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-06-28 18:13:24 +0200

    Support list selection callbacks (#69)
    * Support for QItemSelectionModel as child of list/table/tree views
    
    * Update list example with QItemSelectionModel

--- collagraph/renderers/pyside/objects/listview.py
+++ collagraph/renderers/pyside/objects/listview.py
@@ -1,10 +1,32 @@
+from typing import Union
+
+from PySide6.QtCore import QItemSelectionModel
 from PySide6.QtGui import QStandardItemModel
 
 
-def insert(self, el: QStandardItemModel, anchor=None):
+def insert(self, el: Union[QStandardItemModel, QItemSelectionModel], anchor=None):
+    if isinstance(el, QStandardItemModel):
+        self.setModel(el)
+    elif isinstance(el, QItemSelectionModel):
+        if model := self.model():
+            el.setModel(model)
+            self.setSelectionModel(el)
+        else:
+            raise RuntimeError(
+                "Can't set a selection model because no item model was set (yet). "
+            )
+    else:
+        raise NotImplementedError(type(el).__name__)
+
     el.setParent(self)
-    self.setModel(el)
 
 
-def remove(self, el):
-    self.setModel(None)
+def remove(self, el: Union[QStandardItemModel, QItemSelectionModel]):
+    if isinstance(el, QStandardItemModel):
+        self.setModel(None)
+    elif isinstance(el, QItemSelectionModel):
+        self.setSelectionModel(None)
+    else:
+        raise NotImplementedError(type(el).__name__)
+
+    el.setParent(None)
