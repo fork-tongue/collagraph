5e8cc6a5a12c8f0fffb7b589f3bbc362cb37a60c
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-06-30 10:36:22 +0200

    Add comment to statusbar module

--- collagraph/renderers/pyside/objects/statusbar.py
+++ collagraph/renderers/pyside/objects/statusbar.py
@@ -3,9 +3,14 @@ from . import widget
 
 def insert(self, el, anchor=None):
     # TODO: support for anchor
-    # if anchor:
-    # action = self.insertWidget(anchor, el)
-    # else:
+    # Support for anchor would be tough: there is no API to get
+    # the index of an element, so some custom bookkeeping needs
+    # to take place. But also, when a message is displayed, the
+    # non-permanent widgets seem to get cleared?
+    # And when I tried to remove a permanent widget with a v-if
+    # statement, a non-permanent widget was shown in the
+    # permanent section... so I guess we need a better understanding
+    # of this widget before adding anchor support.
     if getattr(el, "permanent", False):
         self.addPermanentWidget(el)
     else:
@@ -14,12 +19,12 @@ def insert(self, el, anchor=None):
 
 
 def remove(self, el):
+    self.removeWidget(el)
     el.setParent(None)
 
 
 def set_attribute(self, attr, value):
     if attr == "text":
-        # TODO: support for timeout arg?
         if isinstance(value, tuple):
             self.showMessage(*value)
         else:
