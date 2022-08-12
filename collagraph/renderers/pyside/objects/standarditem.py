ec7526c51f0ba987e933a317dc280926ec9bf42d
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-08-12 14:53:00 +0200

    Add support for tree view with hierarchical elements

---
+++ collagraph/renderers/pyside/objects/standarditem.py
@@ -0,0 +1,29 @@
+# from PySide6.QtGui import QStandardItem
+
+
+def insert(self, el, anchor=None):
+    if index := getattr(el, "model_index", None):
+        self.setRowCount(max(self.rowCount(), index[0]))
+        self.setColumnCount(max(self.columnCount(), index[1]))
+        self.setChild(*index, el)
+        return
+
+    if anchor:
+        index = None
+        for row in range(self.rowCount()):
+            if anchor == self.child(row):
+                index = row
+                break
+        if index is None:
+            return
+        self.insertRow(index, el)
+    else:
+        self.appendRow(el)
+
+
+def remove(self, el):
+    if index := getattr(el, "model_index", None):
+        self.takeChild(*index)
+        return
+
+    self.takeRow(el.row())
