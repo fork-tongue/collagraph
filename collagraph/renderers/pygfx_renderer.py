dac43598e9a034e3bf67ab08a4ab321f2f47ac00
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-10-13 14:59:31 +0200

    Allow scale prop to be expressed as int or float

--- collagraph/renderers/pygfx_renderer.py
+++ collagraph/renderers/pygfx_renderer.py
@@ -44,7 +44,10 @@ class PygfxRenderer(Renderer):
     def set_attribute(self, obj, attr, value):
         if isinstance(obj, gfx.WorldObject):
             if attr in {"position", "scale"}:
-                value = gfx.linalg.Vector3(*value)
+                if attr == "scale" and isinstance(value, (int, float)):
+                    value = gfx.linalg.Vector3(value, value, value)
+                else:
+                    value = gfx.linalg.Vector3(*value)
             elif attr == "rotation":
                 value = gfx.linalg.Quaternion(*value)
             elif attr == "matrix":
