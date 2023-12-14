0548cad349a34c031c32ed81aba834632d485274
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2023-12-14 23:23:13 +0100

    Check attr instead of instance

--- collagraph/cgx/cgx.py
+++ collagraph/cgx/cgx.py
@@ -744,9 +744,7 @@ class Element:
 
     def child_with_tag(self, tag):
         for child in self.children:
-            if not isinstance(child, Element):
-                continue
-            if child.tag == tag:
+            if getattr(child, "tag", None) == tag:
                 return child
 
 
