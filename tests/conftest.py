6bb3804eee38618d31f02314d6887e283062c2d5
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2023-12-15 22:42:39 +0100

    Fix deprecation warning in python 3.12

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,13 @@ import asyncio
 
 import pytest
 
+# If there is no current event loop, then get_event_loop()
+# will emit a deprecation warning since Python 3.12, which
+# will be turned into an error for future versions of Python.
+# https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.get_event_loop
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
 
 async def miniloop():
     await asyncio.sleep(0)
