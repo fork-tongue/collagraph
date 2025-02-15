6e4c82c66dc1f8b3803a452fa7604d8f60be3486
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2025-02-15 16:05:57 +0100

    Fix pygfx examples

--- examples/pygfx/combined-example.py
+++ examples/pygfx/combined-example.py
@@ -2,99 +2,12 @@
 Example of how to render lists, tables and trees.
 """
 
-import pygfx as gfx
 from observ import reactive
-from point_cloud import PointCloud, materials, sphere_geom
 from PySide6 import QtWidgets
 from wgpu.gui.qt import WgpuCanvas
 
 import collagraph as cg
-from collagraph import h
-
-
-class RenderWidget(cg.Component):
-    def mounted(self):
-        renderer = gfx.renderers.WgpuRenderer(self.element)
-
-        camera = gfx.PerspectiveCamera(60, 16 / 9)
-        camera.local.z = 15
-        camera.show_pos((0, 0, 0))
-
-        controls = gfx.OrbitController(camera)
-        controls.register_events(renderer)
-
-        self.gui = cg.Collagraph(renderer=cg.PygfxRenderer())
-        element = h(
-            "Group",
-            {
-                "name": "Landmarks",
-            },
-            h("AmbientLight"),
-            h("PointLight"),
-            h(
-                PointCloud,
-                # When increasing this number, it will take longer
-                # and longer for pygfx to create the render pipeline
-                # (compiling shaders and such), so be careful...
-                self.props,
-            ),
-            h(
-                "Mesh",
-                {
-                    "name": "Hip",
-                    "local.position": [2, 2, 2],
-                    "geometry": sphere_geom,
-                    "material": materials["other"],
-                },
-            ),
-        )
-
-        container = gfx.Scene()
-
-        def animate():
-            renderer.render(container, camera)
-
-        self.gui.render(
-            element, container, callback=lambda: self.element.request_draw(animate)
-        )
-
-    def render(self):
-        return h("WgpuCanvas", {"minimum_height": 400, "minimum_width": 600})
-
-
-def Example(props):
-    def add(event):
-        props["count"] += 1
-
-    def remove(event):
-        if props["count"] > 0:
-            props["count"] -= 1
-
-    return h(
-        "Window",
-        {},
-        h(
-            "Widget",
-            {},
-            h(
-                RenderWidget,
-                props,
-            ),
-            h(
-                "Widget",
-                {
-                    "layout": {
-                        "type": "Box",
-                        "direction": "LeftToRight",
-                    },
-                    "maximum-height": 50,
-                },
-                h("Button", {"text": "Add", "on_clicked": add}),
-                h("Button", {"text": "Remove", "on_clicked": remove}),
-            ),
-        ),
-    )
-
+from examples.pygfx.app_point_cloud import Example
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
@@ -103,11 +16,9 @@ if __name__ == "__main__":
     renderer.register_element("WgpuCanvas", WgpuCanvas)
     gui = cg.Collagraph(renderer=renderer)
 
-    state = reactive({"count": 50})
+    state = reactive({"count": 1000})
 
     # Define Qt structure and map state to the structure
-    element = h(Example, state)
-
     # Pass in the app as a container. Can actually be any truthy object
-    gui.render(element, app)
+    gui.render(Example, app, state=state)
     app.exec()
