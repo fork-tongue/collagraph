2147c4e3089a6064c53fd244315934398288d460
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2025-04-07 22:23:14 +0200

    Replace slider example by SFC

--- collagraph/renderers/pyside_renderer.py
+++ collagraph/renderers/pyside_renderer.py
@@ -26,7 +26,7 @@ TYPE_MAPPING = {
     "radiobutton": QtWidgets.QRadioButton,
     "dialogbuttonbox": QtWidgets.QDialogButtonBox,
     "groupbox": QtWidgets.QGroupBox,
-    "progessbar": QtWidgets.QProgressBar,
+    "progressbar": QtWidgets.QProgressBar,
     "scrollarea": QtWidgets.QScrollArea,
     "slider": QtWidgets.QSlider,
     "spinbox": QtWidgets.QSpinBox,
