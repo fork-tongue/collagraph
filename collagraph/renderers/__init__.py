466c2ebf2d2b2e61a63b3d0323eb024fbff147a4
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2025-09-07 22:25:06 +0200

    Fix formatting

--- collagraph/renderers/__init__.py
+++ collagraph/renderers/__init__.py
@@ -82,7 +82,7 @@ try:
 except ImportError:  # pragma: no cover
     pass
 else:
-    from .pyside_renderer import PySideRenderer # noqa I001
+    from .pyside_renderer import PySideRenderer  # noqa I001
 
     # Need to import the objects in order to register their methods
     from .pyside import objects
