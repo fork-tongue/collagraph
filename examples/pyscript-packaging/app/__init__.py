5c965b35282133a09651489eaa558b26b60bb7d6
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2025-09-21 22:07:41 +0200

    Remove pyscript examples

--- examples/pyscript-packaging/app/__init__.py
+++
@@ -1,8 +0,0 @@
-import collagraph as cg
-
-from .app import App
-
-
-def start(container):
-    gui = cg.Collagraph(renderer=cg.DomRenderer())
-    gui.render(cg.h(App), container)
--- examples/pyscript-packaging/main.py
+++
@@ -1,5 +0,0 @@
-import js
-from app import start
-
-container = js.window.document.getElementById("output")
-start(container)
