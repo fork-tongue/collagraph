7f1a1f1e3af517362f7a33154ee4f229a1ebbe6c
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2025-11-03 23:36:18 +0100

    Fix end location of self-closing elements (#132)
    * Fix end location of self-closing elements
    * Bump version to 0.8.4

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,10 @@
+from textwrap import dedent
+
 import pytest
 
 from collagraph import Collagraph, EventLoopType
 from collagraph.renderers import DictRenderer
+from collagraph.sfc.parser import CGXParser
 
 
 def test_parser_unclosed_element(parse_source):
@@ -49,3 +52,28 @@ def test_parser_unclosed_root_element(parse_source):
             </script>
             """
         )
+
+
+def test_parser_element_start_end(parse_source):
+    parser = CGXParser()
+    parser.feed(
+        dedent("""\
+            <long-item
+              @with-many="toet"
+              :attrs="blaat"
+              :that-take="foo"
+              a-lot-of="space"
+            />
+
+            <other />
+        """)
+    )
+
+    assert parser.root
+    long_item = parser.root.children[0]
+    assert long_item.location == (1, 0)
+    assert long_item.end == (6, 0)
+
+    short_item = parser.root.children[1]
+    assert short_item.location == (8, 0)
+    assert short_item.end == (8, 7)
